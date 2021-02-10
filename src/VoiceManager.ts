bc7042aec90fde75967fcb5d0f9f7f841afaac9e
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-02-10 16:01:05 -0500

    Add VoiceText

--- src/VoiceManager.ts
+++ src/VoiceManager.ts
@@ -1,11 +1,19 @@
-import {Message} from "discord.js";
-
+// @ts-ignore
+import { VoiceText } from 'voice-text';
+import { Message } from "discord.js";
+import { createWriteStream } from 'fs';
 export default class VoiceManager {
 
+    static queue = []
+
     static append(message: Message) {
+        if (this.isReadable(message.content)) {
 
+        }
+    }
 
-
+    static isReadable(content: string) {
+        return !(content.length > 30 || new RegExp([":", "http", "@", "#"].join("|")).test(content))
     }
 
 }
--- src/Yuuki.ts
+++ src/Yuuki.ts
@@ -1,15 +1,22 @@
 import Discord from "discord.js";
 import Config from "./Config";
 import VoiceManager from "./VoiceManager";
+import fs from "fs";
 
 export = class Bot extends Discord.Client {
 
     init() {
 
-        Config.load()
+        Config.load();
+
+        ["assets", "assets/voice", "assets/image"].forEach(fs.mkdirSync)
+
         this.login(process.env.DISCORD)
             .then(() => {
                 this.on('message', async message => {
+
+                    if (message.author.bot) return
+
                     if (Object.values(Config.channels!!.text).includes(message.channel.id)) {
                         VoiceManager.append(message)
                     }
