8f9b819f256c1d17c2eb25cd9c69cc07d87cedc1
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-02-17 12:41:27 -0500

    Init

--- src/Yuuki.ts
+++ src/Yuuki.ts
@@ -1,39 +1,20 @@
-import Discord from "discord.js";
-import Config from "./Config";
-import VoiceManager from "./VoiceManager";
-import fs from "fs";
+import {
+    Discord,
+    On,
+    Client,
+    ArgsOf
+} from "@typeit/discord";
 
-export = class Yuuki extends Discord.Client {
+@Discord()
+export abstract class Yuuki {
 
-    init() {
-
-        Config.load();
-        ["assets", "assets/voice", "assets/image"].forEach(it => fs.mkdir(it, () => {}))
-        Config.channels!!.cache = this.channels.cache
-        this.login(process.env.DISCORD)
-            .then(() => {
-
-                setTimeout(this.reboot, 21595000)
-
-                this.on('message', message => {
-                    if (message.author.bot) return
-                    if (Object.values(Config.channels!!.text).includes(message.channel.id)) {
-                        VoiceManager.append(message)
-                    }
-                })
-            })
-
-    }
-
-    reboot() {
-        try {
-            Object.values(Config.channels!!.text).forEach(it => {
-                // @ts-ignore
-                this.channels.cache.get(it)!!.send("再起動します...")
-            })
-        } catch (e) { console.log(e) }
-        console.log("Rebooting...")
-        this.destroy()
+    @On("message")
+    private onMessage(
+        [message]: ArgsOf<"message">,
+        client: Client,
+    ) {
+        console.log(
+        )
     }
 
 }
