e13121059e59ff4591d596d21542b791ed4bffc4
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-02-18 07:29:18 -0500

    Add transliterate

--- src/LocaleManager.ts
+++ src/LocaleManager.ts
@@ -1,11 +1,53 @@
 import { Message } from 'discord.js'
+import { Yuuki } from './Yuuki'
 
 export default class LocaleManager {
 	static queue: Promise<{
 		message: Message
 		transliterated: string
 	}>[] = []
-	static append(message: Message) {}
+
+	static append(message: Message) {
+		if (this.isShouldTransliterate(message.content)) {
+			message.delete()
+			this.queue.push(
+				new Promise(resolve => {
+					fetch(
+						`http://www.google.com/transliterate?langpair=ja-Hira|ja&text=${encodeURIComponent(
+							message.content
+						)}`
+					)
+						.then(res => res.json())
+						.then(it => {
+							resolve({
+								message: message,
+								transliterated: it.map(it => it[1][0]).join('') as string
+							})
+						})
+				})
+			)
+			if (this.queue.length === 1) {
+				this.send()
+			}
+		}
+	}
+
+	static send() {
+		this.queue[0].then(res => {
+			Yuuki.webhookjp
+				.send(res.transliterated, {
+					username: res.message.author.username,
+					avatarURL:
+						res.message.author.avatarURL() ||
+						res.message.author.defaultAvatarURL
+				})
+				.then(() => {
+					if (this.queue.length > 0) {
+						this.send()
+					}
+				})
+		})
+	}
 
 	static isShouldTransliterate(content: string) {
 		return content.match(/^[A-Za-z]+$/)
--- src/Yuuki.ts
+++ src/Yuuki.ts
@@ -3,10 +3,21 @@ import VoiceManager from './VoiceManager'
 import { Discord, On, Client, ArgsOf } from '@typeit/discord'
 import { mkdir } from 'fs'
 import LocaleManager from './LocaleManager'
+import { TextChannel, Webhook } from 'discord.js'
 
 @Discord()
 export class Yuuki {
 	static instance: Client
+	static webhookjp: Webhook
+
+	private static async setState(client: Client) {
+		Yuuki.instance = client
+		Yuuki.webhookjp = (
+			await (<TextChannel>(
+				client.channels.cache.get(Config.channels?.japanese!!)
+			)).fetchWebhooks()
+		).first()!!
+	}
 
 	@On('ready')
 	private onReady() {
@@ -18,14 +29,15 @@ export class Yuuki {
 
 	@On('message')
 	private onMessage([message]: ArgsOf<'message'>, client: Client) {
-		Yuuki.instance = client
-		if (message.author.bot) return
-		if (message.channel.id === Config.channels?.japanese) {
-			LocaleManager.append(message)
-		} else if (
-			Object.values(Config.channels!!.text).includes(message.channel.id)
-		) {
-			VoiceManager.append(message)
-		}
+		Yuuki.setState(client).then(() => {
+			if (message.author.bot) return
+			if (message.channel.id === Config.channels?.japanese) {
+				LocaleManager.append(message)
+			} else if (
+				Object.values(Config.channels!!.text).includes(message.channel.id)
+			) {
+				VoiceManager.append(message)
+			}
+		})
 	}
 }
